42d2ca1c9aa40403804507673cce465d9e83c44e
sanghyeokYun <dev.shyun@gmail.com>
2021-11-17 23:47:21 +0900

    first commit

---
+++ src/main/java/com/kiprisAPI/KiprisApiApplication.java
@@ -0,0 +1,13 @@
+package com.kiprisAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiprisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiprisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiprisAPI/KiprisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiprisAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiprisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
